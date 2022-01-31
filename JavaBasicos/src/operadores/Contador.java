46fb05155b9407a103e42a68ab7e2fced90a32fe
Octavio Olea <oleaoctavio23@gmail.com>
2022-01-31 17:33:43 -0600

    ejercicio resuelto sobre contadores en Java

---
+++ JavaBasicos/src/operadores/Contador.java
@@ -0,0 +1,14 @@
+package operadores;
+
+public class Contador {
+	public static void main(String[] args) {
+		for (int i = 0; i < 100; i++) {
+            if(i>=15&&i<=90) {
+            	if(i%3==0) {
+            		System.out.println(i);
+            	}
+            	
+            }
+		}
+	}
+}
